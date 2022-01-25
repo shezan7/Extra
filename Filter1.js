679be5c2d9c8a872c7077d0009da6d6522eacaf8
shezan7 <bsse1023@iit.du.ac.bd>
2022-01-25 12:03:51 +0600

    25-01-2022 2

--- Filter1.js
+++ Filter1.js
@@ -1,8 +1,6 @@
-var number = [2, 23, 24 , 45, 25, 56, 90];
+let number = [2, 23, 24, 45, 25, 56, 90];
 
-var newNumber = number.filter(function(x){
+let newNumber = number.filter(function(x){
     return x> 25;
-
 })
-
 console.log(newNumber);
--- Map1.js
+++ Map1.js
@@ -1,8 +1,6 @@
-var number = [2, 23, 24 , 45,25];
+let number = [2, 23, 24, 45,25];
 
-var squareNumber = number.map(function(x){
+let squareNumber = number.map(function(x){
     return x*x;
-
 })
-
 console.log(number + " : " + squareNumber);
